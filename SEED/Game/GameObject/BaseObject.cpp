2203c5ae339b352e3632bb2cf5eda919812efe31
Amaimono <k023g0048@g.neec.ac.jp>
2024-12-12 11:23:17 +0900

    Transform の アクセッサ 追加

--- SEED/Game/GameObject/BaseObject.cpp
+++ SEED/Game/GameObject/BaseObject.cpp
@@ -12,3 +12,93 @@ BaseObject::~BaseObject(){}
 void BaseObject::Draw(){
     model_->Draw();
 }
+
+const Vector3& BaseObject::GetScale() const{
+    return model_->scale_;
+}
+
+const Vector3& BaseObject::GetRotate() const{
+    return model_->rotate_;
+}
+
+const Quaternion& BaseObject::GetQuaternion() const{
+    return model_->rotateQuat_;
+}
+
+const Vector3& BaseObject::GetTranslate() const{
+    return model_->translate_;
+}
+
+Vector3 BaseObject::GetWorldPos() const{
+    Matrix4x4 worldMat = model_->GetWorldMat();
+    return  {worldMat.m[3][0],worldMat.m[3][1],worldMat.m[3][2]};
+}
+
+void BaseObject::SetScale(const Vector3& scale){
+    model_->scale_ = scale;
+}
+
+void BaseObject::SetRotate(const Vector3& rotate){
+    model_->rotate_ = rotate;
+}
+
+void BaseObject::SetQuaternion(const Quaternion& quaternion){
+    model_->rotateQuat_ = quaternion;
+}
+
+void BaseObject::SetTranslate(const Vector3& translate){
+    model_->translate_ = translate;
+}
+
+// x, y, z (クォータニオンはwも) の個別セッター
+void BaseObject::SetScaleX(float x){
+    model_->scale_.x = x;
+}
+
+void BaseObject::SetScaleY(float y){
+    model_->scale_.y = y;
+}
+
+void BaseObject::SetScaleZ(float z){
+    model_->scale_.z = z;
+}
+
+void BaseObject::SetRotateX(float x){
+    model_->rotate_.x = x;
+}
+
+void BaseObject::SetRotateY(float y){
+    model_->rotate_.y = y;
+}
+
+void BaseObject::SetRotateZ(float z){
+    model_->rotate_.z = z;
+}
+
+void BaseObject::SetQuaternionX(float x){
+    model_->rotateQuat_.x = x;
+}
+
+void BaseObject::SetQuaternionY(float y){
+    model_->rotateQuat_.y = y;
+}
+
+void BaseObject::SetQuaternionZ(float z){
+    model_->rotateQuat_.z = z;
+}
+
+void BaseObject::SetQuaternionW(float w){
+    model_->rotateQuat_.w = w;
+}
+
+void BaseObject::SetTranslateX(float x){
+    model_->translate_.x = x;
+}
+
+void BaseObject::SetTranslateY(float y){
+    model_->translate_.y = y;
+}
+
+void BaseObject::SetTranslateZ(float z){
+    model_->translate_.z = z;
+}
--- SEED/Game/GameObject/BaseObject.h
+++ SEED/Game/GameObject/BaseObject.h
@@ -6,6 +6,9 @@
 
 class Model;
 
+struct Vector3;
+struct Quaternion;
+
 class BaseObject{
 public:
     BaseObject();
@@ -23,4 +26,70 @@ private:
 public:
     const std::string& GetName()const{ return name_; }
     void SetName(const std::string& _name){ name_ = _name.c_str(); }
+
+    ////////////////////////////////////////////////////
+    //  model_ Transform の アクセッサー
+    ////////////////////////////////////////////////////
+    /// <summary>
+    /// Model's Scale
+    /// </summary>
+    /// <returns></returns>
+    const Vector3& GetScale()const;
+    /// <summary>
+    /// Model's Rotate
+    /// </summary>
+    /// <returns></returns>
+    const Vector3& GetRotate()const;
+    /// <summary>
+    /// Model's RotateQuat
+    /// </summary>
+    /// <returns></returns>
+    const Quaternion& GetQuaternion()const;
+    /// <summary>
+    /// Model's Transform
+    /// </summary>
+    /// <returns></returns>
+    const Vector3& GetTranslate()const;
+    /// <summary>
+    /// Model's WorldPos
+    /// </summary>
+    /// <returns></returns>
+    Vector3 GetWorldPos()const;
+
+    /// <summary>
+    /// モデルのスケールを設定
+    /// </summary>
+    /// <param name="scale">新しいスケール</param>
+    void SetScale(const Vector3& scale);
+    void SetScaleX(float x);
+    void SetScaleY(float y);
+    void SetScaleZ(float z);
+
+    /// <summary>
+    /// モデルの回転を設定
+    /// </summary>
+    /// <param name="rotate">新しい回転</param>
+    void SetRotate(const Vector3& rotate);
+    void SetRotateX(float x);
+    void SetRotateY(float y);
+    void SetRotateZ(float z);
+
+    /// <summary>
+    /// モデルのクォータニオンを設定
+    /// </summary>
+    /// <param name="quaternion">新しいクォータニオン</param>
+    void SetQuaternion(const Quaternion& quaternion);
+    void SetQuaternionX(float x);
+    void SetQuaternionY(float y);
+    void SetQuaternionZ(float z);
+    void SetQuaternionW(float w);
+
+    /// <summary>
+    /// モデルの位置を設定
+    /// </summary>
+    /// <param name="translate">新しい位置</param>
+    void SetTranslate(const Vector3& translate);
+    void SetTranslateX(float x);
+    void SetTranslateY(float y);
+    void SetTranslateZ(float z);
 };

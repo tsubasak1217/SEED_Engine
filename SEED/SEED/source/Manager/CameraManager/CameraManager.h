775298e6d7595ba30cf98a1ff1876a85383f4afa
kase-haruto <0te06559726481t@au.com>
2025-01-09 12:28:39 +0900

    activeなカメラのみを更新するようにしました

--- SEED/SEED/source/Manager/CameraManager/CameraManager.h
+++ SEED/SEED/source/Manager/CameraManager/CameraManager.h
@@ -31,6 +31,11 @@ public:
     static BaseCamera* GetCamera(const std::string& name);
     static void AddCamera(const std::string& name, BaseCamera* camera);
     static void DeleteCamera(const std::string& name);
+    static BaseCamera* GetActiveCamera();
+
+    // アクティブなカメラを設定
+    static void SetActiveCamera(const std::string& name);
+
 
 private:
 
@@ -42,4 +47,7 @@ private:
     // メインのカメラ
     std::unique_ptr<BaseCamera> mainCamera_;
     std::unique_ptr<DebugCamera> debugCamera_;
+
+    // アクティブなカメラを保持するポインタ
+    BaseCamera* activeCamera_ = nullptr;
 };

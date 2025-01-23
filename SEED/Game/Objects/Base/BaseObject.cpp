6583e8e2d8c19cc8fd5f6a8885678e7ee4653260
tsubasak1217 <k023g0112@g.neec.ac.jp>
2025-01-23 10:10:39 +0900

    ジャンプ今度こそ直った

--- SEED/Game/Objects/Base/BaseObject.cpp
+++ SEED/Game/Objects/Base/BaseObject.cpp
@@ -126,7 +126,7 @@ void BaseObject::EndFrameDropFlagUpdate(){
             return;
         }
 
-        dropSpeed_ += Physics::kGravity / 60.0f;
+        dropSpeed_ += (Physics::kGravity / 60.0f) * ClockManager::TimeRate();
     }
 }
 

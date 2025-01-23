bee53bf65269730045dcc52b2125fae5e7d38f41
kase-haruto <0te06559726481t@au.com>
2025-01-23 11:01:01 +0900

    ルーチンポイントをボタンで設置できるようにしました

--- SEED/Game/Editor/EnemyEditor.h
+++ SEED/Game/Editor/EnemyEditor.h
@@ -1,10 +1,14 @@
 #pragma once
 
 // local 
+#include "Vector3.h"
+#include "FieldObject/FieldObject.h"
 
 // lib
 #include <string>
 #include <cstdint>
+#include <vector>
+#include <memory>
 
 class EnemyManager;
 
@@ -21,9 +25,19 @@ private:
     void LoadEnemies();
 
 private:
+    //---　マウスによるルーチンポイント追加
+    void AddRoutinePointByMouse();
+
+    //オブジェクトのインデックスを取得
+    Vector3 GetObjectPositionByMouse(const std::vector<std::unique_ptr<FieldObject>>& objects);
+    int32_t GetObjectIndexByMouse(std::vector<std::unique_ptr<FieldObject>>& objects);
+
+private:
     int selectedEnemyIndex_ = 0;
     int32_t enemyCount_ = 0;
 
     char nameBuf_[128] = "";        // 選択中の敵の名前編集用バッファ
     int lastSelectedIndex_ = -1;    // 最後に選択された敵のインデックス
+
+    std::string selectedRoutineName = ""; // 選択中のルーチン名
 };

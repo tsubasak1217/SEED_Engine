427a2058fceba4827c2c564087617c227e50801a
kase-haruto <0te06559726481t@au.com>
2025-01-28 11:37:39 +0900

    敵を食べた時に出るエラーを修正

--- SEED/Game/Objects/Player/PlayerState/PlayerState_Eat.cpp
+++ SEED/Game/Objects/Player/PlayerState/PlayerState_Eat.cpp
@@ -17,7 +17,7 @@
 #include "MyMath.h"
 
 PlayerState_Eat::PlayerState_Eat(BaseCharacter* player){
-    Initialize("PlayerState_Eat",player);
+    Initialize("PlayerState_Eat", player);
 }
 
 PlayerState_Eat::~PlayerState_Eat(){}
@@ -30,11 +30,11 @@ void PlayerState_Eat::Update(){
 
 void PlayerState_Eat::Draw(){}
 
-void PlayerState_Eat::Initialize(const std::string& stateName,BaseCharacter* player){
-    ICharacterState::Initialize(stateName,player);
+void PlayerState_Eat::Initialize(const std::string& stateName, BaseCharacter* player){
+    ICharacterState::Initialize(stateName, player);
 
     { // 捕食対象の取得
-        Player* pPlayer = dynamic_cast<Player*>(pCharacter_);
+        Player* pPlayer = dynamic_cast< Player* >(pCharacter_);
         // 捕食対象リストの先頭を取得(一番近い敵)
         enemy_ = pPlayer->GetPredationRange()->GetPreyList().front().enemy;
     }
@@ -42,31 +42,32 @@ void PlayerState_Eat::Initialize(const std::string& stateName,BaseCharacter* pla
     {
         // 捕食対象の方向を向く
         Vector3 direction = enemy_->GetWorldTranslate() - pCharacter_->GetWorldTranslate();
-        interpolationRotateY_ = atan2f(direction.x,direction.z);
+        interpolationRotateY_ = atan2f(direction.x, direction.z);
+
     }
 
-    currentUpdate_ = [this](){RotateForEnemy(); };
+    currentUpdate_ = [this] (){RotateForEnemy(); };
 }
 
 void PlayerState_Eat::ManageState(){}
 
 void PlayerState_Eat::RotateForEnemy(){
     float t = currentTime_ / rotateTime_;
-    float interpolatedRotateY = MyMath::LerpShortAngle(pCharacter_->GetLocalRotate().y,interpolationRotateY_,std::clamp(t,0.f,1.f));
+    float interpolatedRotateY = MyMath::LerpShortAngle(pCharacter_->GetLocalRotate().y, interpolationRotateY_, std::clamp(t, 0.f, 1.f));
     pCharacter_->SetRotateY(interpolatedRotateY);
 
-    if(t >= 1.0f){
+    if (t >= 1.0f){
         currentTime_ = 0.f;
-        currentUpdate_ = [this](){EatEnemy(); };
+        currentUpdate_ = [this] (){EatEnemy(); };
     }
 }
 
 void PlayerState_Eat::EatEnemy(){
     float t = currentTime_ / eatTime_;
     // なんかアニメーションが流れる
-    if(t >= 1.0f){
+    if (t >= 1.0f){
         currentTime_ = 0.f;
-        currentUpdate_ = [this](){SpawnEgg(); };
+        currentUpdate_ = [this] (){SpawnEgg(); };
 
         // 敵を削除
         enemy_->SetIsAlive(false);
@@ -79,8 +80,8 @@ void PlayerState_Eat::SpawnEgg(){
     float t = currentTime_ / spawnEggTime_;
     // なんかアニメーションが流れる
 
-    if(t >= 1.0f){
-        Player* pPlayer = dynamic_cast<Player*>(pCharacter_);
+    if (t >= 1.0f){
+        Player* pPlayer = dynamic_cast< Player* >(pCharacter_);
         // 卵を生成
         {
             std::unique_ptr<Egg> spawnedEgg = std::make_unique<Egg>(pPlayer);
@@ -89,6 +90,6 @@ void PlayerState_Eat::SpawnEgg(){
         }
 
         // ステートを変更
-        pCharacter_->ChangeState(new PlayerState_Idle("PlayerState_Idle",pCharacter_));
+        pCharacter_->ChangeState(new PlayerState_Idle("PlayerState_Idle", pCharacter_));
     }
 }

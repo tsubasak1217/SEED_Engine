18ae006a13e56cd135d658b8e22d905ce686df62
tsubasak1217 <k023g0112@g.neec.ac.jp>
2025-02-05 15:18:36 +0900

    チュートリアルを継承した形に直した

---
+++ SEED/Game/Scene/Scene_Game/State/EventState/EventState_Tutorial_Base.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "EventState_Base.h"
+#include "Scene_Game.h"
+#include "../PlayerInput/PlayerInput.h"
+
+class EventState_Tutorial_Base : public EventState_Base{
+public:
+    EventState_Tutorial_Base();
+    EventState_Tutorial_Base(Scene_Base* pScene);
+    virtual ~EventState_Tutorial_Base()override;
+    virtual void Initialize(Scene_Base* pScene);
+    virtual void Update()override;
+    virtual void Draw()override;
+
+protected:
+    Scene_Game* pGameScene_;
+    FollowCamera* pCamera_;
+    Player* pPlayer_;
+
+protected:
+    float time_;
+    float kTime_;
+    int32_t textStepMax_;
+    int32_t textStep_;
+    float spriteClipHeightOffset_;
+
+    static std::unique_ptr<Sprite> textFieldSprite_;
+    static std::unique_ptr<Sprite> textSprite_;
+};

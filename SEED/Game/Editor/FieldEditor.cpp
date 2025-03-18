319ae89fa141cfcd275be02c9dcf6cfc0c4571df
tsubasak1217 <k023g0112@g.neec.ac.jp>
2025-03-18 16:21:51 +0900

    fieldEditorのドロップダウン選択でステージ10まで選択できるように

--- SEED/Game/Editor/FieldEditor.cpp
+++ SEED/Game/Editor/FieldEditor.cpp
@@ -476,11 +476,11 @@ void FieldEditor::ShowImGui(){
             "3\0"
             "4\0"
             "5\0"
-            "6\0"/*
+            "6\0"
             "7\0"
             "8\0"
             "9\0"
-            "10\0\0"*/
+            "10\0\0"
             )){
             manager_.SetCurrentStageNo(edittingStageIndex);
         }

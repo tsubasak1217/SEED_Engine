61479637fa5dddc960705233a37783bd14755b7c
IshikawaTakeshi <146906463+IshikawaTakeshi@users.noreply.github.com>
2025-09-03 14:34:23 +0900

    GameState_Clear追加

---
+++ SEED/GameState_Clear.cpp
@@ -0,0 +1,25 @@
+#include "GameState_Clear.h"
+
+GameState_Clear::GameState_Clear(Scene_Base* pScene) {
+
+    pScene_ = pScene;
+}
+
+GameState_Clear::~GameState_Clear() {}
+
+void GameState_Clear::Initialize() {}
+
+void GameState_Clear::Finalize() {}
+
+void GameState_Clear::Update() {}
+
+void GameState_Clear::Draw() {}
+
+void GameState_Clear::BeginFrame() {}
+
+void GameState_Clear::EndFrame() {}
+
+void GameState_Clear::HandOverColliders() {}
+
+void GameState_Clear::ManageState() {}
+

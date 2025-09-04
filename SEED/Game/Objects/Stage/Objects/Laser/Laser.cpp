a43d74fdddf1e2b80de2abad871988b9976333a8
IshikawaTakeshi <146906463+IshikawaTakeshi@users.noreply.github.com>
2025-09-04 15:28:21 +0900

    Laserクラスの追加

---
+++ SEED/Game/Objects/Stage/Objects/Laser/Laser.cpp
@@ -0,0 +1,28 @@
+#include "Laser.h"
+
+void Laser::Initialize(const std::string& filename) {
+
+    // スプライトの初期化
+    sprite_ = Sprite(filename);
+    sprite_.anchorPoint = Vector2(0.5f);
+}
+
+void Laser::Update() {
+
+
+    // オブジェクトの更新を行う
+    switch (commonState_) {
+    case StageObjectCommonState::None:
+        break;
+    case StageObjectCommonState::Hologram:
+
+        sprite_.color = Vector4(1.0f, 0.0f, 0.0f, 1.0f);
+        break;
+    }
+}
+
+void Laser::Draw() {
+
+    // スプライトの描画
+    sprite_.Draw();
+}
